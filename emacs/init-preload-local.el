de522863b66a3908f61af40c773eaf0c80df46dc
Zeyu Chen <zeyu.chen@btcc.com>
2016-07-21 20:47:29 +0800

    feat(emacs): add emacs

---
+++ emacs/init-preload-local.el
@@ -0,0 +1,10 @@
+;;; package --- light
+;;; Commentary:
+;;; Code:
+
+(setq package-archives '(("gnu"   . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+			 ("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize) ;; You might already have this line
+
+(provide 'init-preload-local)
+;;; init-preload-local.el ends here
